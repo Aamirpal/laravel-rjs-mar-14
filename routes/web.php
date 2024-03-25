76fbeecea12a4f63fadb352ed97827fed407a575
Aamirpal Sehmi <aamirpals@bridgeshealth.com>
2024-03-25 08:03:55 -0600

    mock route added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('customers', function () {
+    //call mock api to get customers
+     $response = Http::get('https://65efc415ead08fa78a50e705.mockapi.io/api/customers');
+     return $response->json();
 });
